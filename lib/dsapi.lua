e58e7257ff6ae9ae3453850977c44e2a6a533353
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2016-02-26 16:54:44 +0100

    Naprawiono błędy.

--- lib/dsapi.lua
+++ lib/dsapi.lua
@@ -66,8 +66,9 @@
 
 local hdp = require("hdp")
 local serial = require("serialization")
+local fs = require("filesystem")
 
-local wersja = "2.0"
+local wersja = "2.1"
 
 local aa, bb = require("shell").parse(...)
 if aa[1] == "version_check" then return wersja end
@@ -90,7 +91,8 @@ local respCode = {
 	nomem = {0x02, "za malo pamieci na serwerze"}, -- za ma³o miejsca na serwerze
 	notfound = {0x03, "element nie zostal odnaleziony"}, -- nie znaleziono pliku lub folderu
 	badreq = {0x04, "bledne zapytanie"}, -- zapytanie jest niekompletne lub nie istnieje
-	echo = {0x10, "echo"},
+	echo = {0x10, "echo"}, -- echo
+	badname = {0x11, "rozszerzenie nie moze skladac sie z cyfr"}
 }
 
 local dsapi = {}
@@ -103,7 +105,7 @@ function getPort(port)
 	return localPort
 end
 
-function dsapi.list(port, path)
+function dsapi.list(port, path, split)
 	local localPort = getPort(port)
 	local status, code = hdp.send(port, localPort, serial.serialize({reqCode.list, path}))
 	if status then
@@ -112,10 +114,32 @@ function dsapi.list(port, path)
 			local tab = serial.unserialize(input[7])
 			if tab ~= nil then
 				if tab[1] == respCode.success[1] then
+					iter = tab[2]
+					iter2 = {}
+					if not split then
+						for a, b in pairs(iter) do
+							local result = b[1]:match("%.%d+$")
+							if result then
+								local found = false
+								for ind, pos in pairs(iter2) do
+									if pos[1] == b[1]:sub(1, -result:len() - 1) then
+										iter[ind][2] = iter[ind][2] + b[2]
+										found = true
+										break
+									end
+								end
+								if not found then
+									table.insert(iter2, {result, b[2]})
+								end
+							else
+								table.insert(iter2, {b[1], b[2]})
+							end
+						end
+					end
 					local i = 0
 					return true, function()
 						i = i + 1
-						if i <= #tab[2] then return tab[2][i][1], tab[2][i][2] end
+						if i <= #iter then return iter[i][1], iter[i][2] end
 					end
 				else
 					return false, tab[1]
@@ -156,7 +180,11 @@ local function doWrite(port, path, content)
 end
 
 function dsapi.write(port, path, content)
-	if content:len() > 10240 then
+	local segments = fs.segments(path or "")
+	if segments[#segments]:match("%.%d+$") then
+		return false, respCode.badname[1]
+	end
+	if content:len() > 10240 and false then
 		local parts = math.ceil(content:len() / 10240)
 		for part = 1, parts do
 			local suffix = "." .. (part < 10 and ("0" .. tostring(part)) or tostring(part))
@@ -172,18 +200,21 @@ end
 
 local function getSegments(port, path)
 	local subpath = "/"
-	local segments = require("filesystem").segments(path)
+	local segments = fs.segments(path or "")
 	for i = 1, #segments - 1 do
 		subpath = subpath .. "/" .. segments[i]
 	end
-	local s, i = dsapi.list(port, subpath)
+	local s, i = dsapi.list(port, subpath, true)
 	if s then
 		local list = {}
 		for name, size in i do
 			if name == segments[#segments] then return true, {subpath .. "/" .. name} end
-			if name:match("^" .. segments[#segments] .. ".%d%d$") then
+			if name:match("^" .. segments[#segments] .. "%.%d+$") then
 				table.insert(list, subpath .. "/" .. name)
 			end
+		end
+		if #list == 0 then
+			return false, respCode.notfound[2]
 		end
 		return true, list
 	else
@@ -264,7 +295,7 @@ end
 
 function dsapi.translateCode(code)
 	if code ~= nil then
-		for _, obj in ipairs(respCode) do
+		for _, obj in pairs(respCode) do
 			if code == obj[1] then return obj[2] end
 		end
 		return hdp.translateMessage(code)
